4a27289e52cfcbe4af87c6e8b724eb8dfb42a64d
Nicolas Ramel <nramel@preview-communication.fr>
2019-01-23 12:09:47 +0100

    rename observer

--- Observer/ComZeappsContractObserver.php
+++ Observer/ComZeappsContractObserver.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace App\fr_soca_production\Observer;
+namespace App\com_zeapps_contract\Observer;
 
 use Zeapps\Core\iObserver ;
 use Zeapps\Models\CronModel;
 
-class SocaObserver implements iObserver
+class ComZeappsContractObserver implements iObserver
 {
     public static function action($transmitterClassName = '', $actionName = '', $arrayParam = array(), $callBack = null) {
 
-        if ($transmitterClassName == 'com_zeapps_crm' && $actionName == 'save') {
+        /*if ($transmitterClassName == 'com_zeapps_crm' && $actionName == 'save') {
             echo "ok contact observer<br>" ;
-        }
+        }*/
 
 
     }
@@ -29,11 +29,11 @@ class SocaObserver implements iObserver
         $retour = array();
 
         // déclaration du cron : executé tous les jours à 4h00
-        $cron = new CronModel() ;
+        /*$cron = new CronModel() ;
         $cron->minute = "0" ;
         $cron->hour = "4" ;
-        $cron->command = "App\\fr_soca_production\\Controllers\\Crons@checkEffectifs" ;
-        $retour[] = $cron ;
+        $cron->command = "App\\com_zeapps_contract\\Controllers\\Crons@checkEffectifs" ;
+        $retour[] = $cron ;*/
 
         return $retour ;
     }
